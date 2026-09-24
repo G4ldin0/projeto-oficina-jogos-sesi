3f9929c3bb8d3570ce187ce8b76ef00a0358a9c2
João Lucas <joao.duarte31219@alunos.ufersa.edu.br>
2026-09-24 17:49:28 -0300

    gerenciador de salas

---
+++ dinossauros-vs-escadas/elementos/mapa/gerenciador_de_mapa.gd
@@ -0,0 +1,43 @@
+extends Node2D
+class_name GerenciadorDeMapa
+
+var salas:Array[Sala]
+var salas_concluidas:Array[Sala]
+@onready var camera:Camera2D = %Camera2D
+@onready var jogador:Player = %Jogador
+
+@export var sala_inicial:Sala
+
+func _ready():
+	jogador.position = sala_inicial.position
+	camera.position = sala_inicial.position
+	
+	for sala in $salas.get_children():
+		salas.append(sala)
+		if sala == sala_inicial:
+			continue
+		for inimigo in sala.inimigos:
+			inimigo.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func ativar_sala(sala:Sala):
+	for inimigo in sala.inimigos:
+		inimigo.process_mode = Node.PROCESS_MODE_INHERIT
+
+func trocar_sala(porta:Area2D) -> void:
+	var sala_alvo:Sala
+	for sala in salas:
+		if sala.portas.has(porta):
+			sala_alvo = sala
+			break
+	
+	if sala_alvo == null: 
+		print('sala não encontrada')
+		return
+	
+	#jogador.process_mode = Node.PROCESS_MODE_DISABLED
+	await create_tween().tween_property(camera, "position", sala_alvo.global_position, 2.0).finished
+	#jogador.process_mode = Node.PROCESS_MODE_INHERIT
+	ativar_sala(sala_alvo)
+	
+	jogador.position = porta.global_position + (sala_alvo.global_position - porta.global_position).normalized() * 150.0
